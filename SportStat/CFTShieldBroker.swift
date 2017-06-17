9dbf06d0ec381724e868c4dd03b74f380f6387d1
Collin Thommasen <collinthommasen@Collins-MacBook-Air.local>
2017-06-17 13:57:56 -0700

    Commit the Shield Broker class.

---
+++ SportStat/CFTShieldBroker.swift
@@ -0,0 +1,9 @@
+//
+//  CFTShieldBroker.swift
+//  SportStat
+//
+//  Created by Collin Thommasen on 2017-06-17.
+//  Copyright © 2017 Collin Thommasen. All rights reserved.
+//
+
+import Foundation
